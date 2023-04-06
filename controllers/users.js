e38677a687132602b55352c52e75a164e5551a42
username <youremail@domain.com>
2023-04-06 12:28:47 -0700

    adds server and leaves route pseudocode

---
+++ controllers/users.js
@@ -0,0 +1,27 @@
+// required packages
+const express = require('express')
+const router = express.Router()
+
+//mount routes on router
+
+// GET users/new -- show route for a form that creates a new user(sign up for the app)
+
+
+//POST /users -- create a new user from the form at GET/users/new
+
+
+//GET user/login -- show route for a form that lets a user login
+
+
+//POST /user/login -- authenticate a user's credentials 
+
+
+// GET /users/logout -- log out the current user 
+
+//GET /users/profile -- show authorized users their profile page
+
+
+
+
+//export the router instance
+module.exports = router 
---
+++ index.js
@@ -0,0 +1,24 @@
+// required packages
+require('dotenv').config()
+const express = require('express')
+
+//app config 
+const app = express()
+const PORT = process.env.PORT || 8000
+app.set('view engine', 'ejs')
+
+//parse html form request bodies 
+//middleware
+app.use(express.urlencoded({extended: false}))
+
+// routes and controllers
+app.get('/', (req, res)=>{
+    res.send('welcome to the auth api')
+})
+
+app.use('/users', require('./controllers/users.js'))
+
+//listen on a port
+app.listen(PORT, () =>{
+    console.log(`authenticating users on port ${PORT}`)
+})

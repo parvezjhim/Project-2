d7ad1c088186d1d13532dce22cd35be47150d690
username <youremail@domain.com>
2023-04-07 10:42:07 -0700

    code along with weston

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,9 @@
 // required packages
 const express = require('express')
 const router = express.Router()
+const db = require('../models')
+const bcrypt = require('bcrypt')
+const cryptoJs = require('crypto-js')
 
 //mount routes on router
 
@@ -10,24 +13,46 @@ router.get('/new', (req, res) =>{
 })
 
 //POST /users -- create a new user from the form at GET/users/new
-router.post('/', (req, res)=>{
-   //do a findOrCreate with the user's given email
-    // if the user's returns as found -- don't let them sign up 
-    //instead re direct to the login page
-    // hash the user's password before we add it ot the db 
-    // save the user in the db
-    //encrypt the log in user's id 
-    //set encrypted id as a cookie 
-    // redirect the user 
+router.post('/', async (req, res)=>{
+    try{
+        //do a findOrCreate with the user's given email
+        const [newUser, created] = await db.user.findOrCreate({
+            where:{
+                email: req.body.email
+            }
+        })
+        if(!created){
+            // if the user's returns as found -- don't let them sign up 
+            console.log('user account exists')
+            //instead re direct to the login page
+            res.redirect('/users/login?message=Please login to your account to continue')
+        } else{
+            // hash the user's password before we add it ot the db 
+            const hashedPassed = bcrypt.hashSync(req.body.password, 12)
+            // save the user in the db
+            newUser.password = hashedPassed
+            await newUser.save()
+            //encrypt the log in user's id 
+            const encryptedPk = cryptoJs.AES.encrypt(newUser.id.toString(), process.env.ENC_KEY)
+            //set encrypted id as a cookie 
+            res.cookie('userId', encryptedPk.toString())
+            // redirect the user 
+            res.redirect('/users/profile')
+        }
 
 
-   
-    res.send('create a new user if they do not exist already in the db, log a user in')
+    }catch(err){
+        console.log(err)
+        res.redirect('/')
+    }
+
 })
 
 //GET user/login -- show route for a form that lets a user login
 router.get('/login', (req, res)=>{
-    res.send("show a form to let a user login")
+    res.render('users/login.ejs', {
+        message: req.query.message ? req.query.message : null
+    })
 })
 
 //POST /user/login -- authenticate a user's credentials 

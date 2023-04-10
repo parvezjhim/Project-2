e1a5fb80bf8fc0416ec8e1fc4d101ee9e867592c
username <youremail@domain.com>
2023-04-10 11:00:33 -0700

    running into an error with controllers

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,5 +1,5 @@
-// required packages
 const express = require('express')
+const app = express()
 const router = express.Router()
 const db = require('../models')
 const bcrypt = require('bcrypt')
@@ -7,47 +7,38 @@ const cryptoJs = require('crypto-js')
 const axios = require('axios')
 
 
-// const axios = require('axios')
-
-// // api key 4bCbeJXvkhqCa2ZSgxTc5665AP7g2TqU5FdJf5PgCgP1nx9KGC
-// const CLIENT_ID = 'WQdoenR5rS0zAP40HzU4ZM8CLm0FI8F2ZoWQ3QzJ50fpPhC2Ad'
-// // secret HNc7vJ8SFtB4jtB0OqH47vV4gTp5VbP3FtlKRg6z
-// const SECRET = 'Yi49YezgXhDsmJC6inVvqe21EznQ53BXxcAZiqsi'
-
-// async function fetchPets() {
-//     try {
-//         const body = {
-//             'grant_type': 'client_credentials',
-//             'client_id': CLIENT_ID,
-//             'client_secret': SECRET
-//         }
-
-//         // https://api.petfinder.com/v2/oauth2/token
-//         const tokenUrl = 'https://api.petfinder.com/v2/oauth2/token'
-//         const tokenResponse = await axios.post(tokenUrl, body)
-//         console.log('bearer token reponse:', tokenResponse.data)
-
-//         const options = {
-//             headers: {
-//                 Authorization: `Bearer ${tokenResponse.data.access_token}`
-//             }
-//         }
-
-//         const dataResponse = await axios.get('https://api.petfinder.com/v2/animals?type=dog&page=2', options)
-
-//         console.log('data response:', dataResponse.data)
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
-
-// fetchPets()
-
-//routes
 router.get('/animals', (req, res)=>{
-    res.send('view animals')
+    try{
+        res.render('../views/animals.ejs')
+    } catch(err){
+        console.log(err)
+        res.send(err)
+    }
 })
 
+router.post('/animals', async (req, res)=>{
+    try {
+        const { zipCode, species, breed } = req.body
+        const tokenResponse = await axios.get('https://api.petfinder.com/v2/oauth2/token', {
+          grant_type: 'client_credentials',
+          client_id: 'WQdoenR5rS0zAP40HzU4ZM8CLm0FI8F2ZoWQ3QzJ50fpPhC2Ad',
+          client_secret: 'Yi49YezgXhDsmJC6inVvqe21EznQ53BXxcAZiqsi'
+        })
+    
+        const options = {
+          headers: {
+            Authorization: `Bearer ${tokenResponse.data.access_token}`
+          }
+        }
+    
+        const petResponse = await axios.get(`https://api.petfinder.com/v2/animals?type=${species}&breed=${breed}&location=${zipCode}`, options)
+        res.render('animals.ejs', { pets: petResponse.data.animals })
+      } catch (error) {
+        console.log(error)
+        res.render('animals.ejs', { pets: null, error: 'Error getting pets. Please try again.' })
+      }
+    })
+    
 router.get('/animals/:species', (req, res)=>{
     res.send('view one specific species')
 })
@@ -69,4 +60,5 @@ router.delete('/users/:userId/animals/:animalId', (req, res) =>{
 })
 
 
-module.exports= router
+
+module.exports = router;
